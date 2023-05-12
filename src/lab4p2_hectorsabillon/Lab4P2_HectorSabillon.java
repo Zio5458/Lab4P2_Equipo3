3e49d2794cd15dd3ae4f069427b9f50c2fa14e0f
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-05-12 13:36:53 -0600

    Main

--- src/lab4p2_hectorsabillon/Lab4P2_HectorSabillon.java
+++ src/lab4p2_hectorsabillon/Lab4P2_HectorSabillon.java
@@ -1,12 +1,32 @@
 
 package lab4p2_hectorsabillon;
 
+import java.util.Scanner;
+
 
 public class Lab4P2_HectorSabillon {
 
     
     public static void main(String[] args) {
-        
+        Scanner pochita = new Scanner(System.in);
+        int opc = 0;
+        while (opc != 3) {            
+            System.out.print("---Inicio---\n\n1.Iniciar Sesion\n2.Crear Usuario\n3.Salir\nElegir una opcion: ");
+            opc = pochita.nextInt();
+            switch (opc) {
+                case 1:
+                    
+                    break;
+                    //fin opc 1
+                case 2:
+                    break;
+                    // fin opc 2
+                case 3:
+                    //fin opc 3
+                default:
+                    throw new AssertionError();
+            }
+        }
         
     }
     
