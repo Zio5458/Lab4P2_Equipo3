101b7379260c1fcfd839eaba238e95ae94de144d
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-05-12 14:25:15 -0600

    Crear usuarios y mitad de modificar

--- src/lab4p2_hectorsabillon/Lab4P2_HectorSabillon.java
+++ src/lab4p2_hectorsabillon/Lab4P2_HectorSabillon.java
@@ -10,15 +10,191 @@ public class Lab4P2_HectorSabillon {
     public static void main(String[] args) {
         Scanner pochita = new Scanner(System.in);
         int opc = 0;
-        while (opc != 3) {            
-            System.out.print("---Inicio---\n\n1.Iniciar Sesion\n2.Crear Usuario\n3.Salir\nElegir una opcion: ");
+        while (opc != 6) {            
+            System.out.print("---Menu---\n\n"
+                    + "1.Crear\n"
+                    + "2.Modificar\n"
+                    + "3.Listar\n"
+                    + "4.Eliminar\n"
+                    + "5.Viajar\n"
+                    + "6.Salir\n"
+                    + "Elegir una opcion: ");
             opc = pochita.nextInt();
+            System.out.println("");
             switch (opc) {
                 case 1:
-                    
+                    System.out.print("---Crear---\n\n"
+                            + "1.Crear Ingeniero\n"
+                            + "2.Crear Primate\n"
+                            + "3.Crear Planeta\n"
+                            + "4.Crear Transporte Aereo Normal\n"
+                            + "5.Crear Transporte Espacial\n"
+                            + "Elegir una opcion: ");
+                    pochita = new Scanner(System.in);
+                    opc = pochita.nextInt();
+                    switch (opc) {
+                        case 1:
+                            
+                            break;
+                            //fin opc 1
+                        case 2:
+                            break;
+                            //fin opc 2
+                        case 3:
+                            break;
+                            //fin opc 3
+                        case 4:
+                            break;
+                            //fin opc 4.
+                        case 5:
+                            break;
+                            //fin opc 5
+                        default:
+                            throw new AssertionError();
+                    }
                     break;
                     //fin opc 1
                 case 2:
+                    System.out.print("---Modificar---\n\n"
+                            + "1.Modificar Ingeniero\n"
+                            + "2.Modificar Primate\n"
+                            + "3.Modificar Planeta\n"
+                            + "4.Modificar Transporte Aereo Normal\n"
+                            + "5.Modificar Transporte Espacial\n"
+                            + "Elegir una opcion: ");
+                    pochita = new Scanner(System.in);
+                    opc = pochita.nextInt();
+                    switch (opc) {
+                        case 1:
+                            System.out.print("---Ingeniero---\n\n"
+                            + "1.Modificar numero de RRHH\n"
+                            + "2.Modificar Usuario\n"
+                            + "3.Modificar Contrasena\n"
+                            + "4.Modificar Nombre\n"
+                            + "5.Modificar Grupo Sanguineo\n"
+                            + "6.Modificar Sexo\n"
+                            + "7.Modificar Altura\n"
+                            + "8.Modificar Peso\n"
+                            + "Elegir una opcion: ");
+                            pochita = new Scanner(System.in);
+                            opc = pochita.nextInt();
+                            System.out.println("");
+                            switch (opc) {
+                                case 1:
+                                    
+                                    break;
+                                    //fin opc 1
+                                case 2:
+                                    break;
+                                    //fin opc 2
+                                case 3:
+                                    break;
+                                    //fin opc 3
+                                case 4:
+                                    break;
+                                    //fin opc 4
+                                case 5:
+                                    break;
+                                    //fin opc 5
+                                case 6:
+                                    break;
+                                    //fin opc 6
+                                case 7:
+                                    break;
+                                    //fin opc 7
+                                case 8:
+                                    break;
+                                    //fin opc 8
+                                default:
+                                    throw new AssertionError();
+                            }
+                            break;
+                            //fin opc 1
+                        case 2:
+                            System.out.print("---Primate---\n\n"
+                            + "1.Modificar Medio de Transporte Asignado\n"
+                            + "2.Modificar Cantidad de Comida\n"
+                            + "3.Modificar Cuanto Come por KM\n"
+                            + "4.MOdificar Lugar de Nacimiento\n"
+                            + "5.Modificar Nombre\n"
+                            + "6.Modificar Grupo Sanguineo\n"
+                            + "7.Modificar Sexo\n"
+                            + "8.Modificar Altura\n"
+                            + "9.Modificar Peso\n"
+                            + "Elegir una opcion: ");
+                            pochita = new Scanner(System.in);
+                            opc = pochita.nextInt();
+                            System.out.println("");
+                            switch (opc) {
+                                case 1:
+                                    
+                                    break;
+                                    //fin opc 1
+                                case 2:
+                                    break;
+                                    //fin opc 2
+                                case 3:
+                                    break;
+                                    //fin opc 3
+                                case 4:
+                                    break;
+                                    //fin opc 4
+                                case 5:
+                                    break;
+                                    //fin opc 5
+                                case 6:
+                                    break;
+                                    //fin opc 6
+                                case 7:
+                                    break;
+                                    //fin opc 7
+                                case 8:
+                                    break;
+                                    //fin opc 8
+                                case 9:
+                                    break;
+                                    //fin opc 9
+                                default:
+                                    throw new AssertionError();
+                            }
+                            break;
+                            //fin opc 2
+                        case 3:
+                            System.out.print("---Planeta---\n\n"
+                            + "1.Modificar Nombre\n"
+                            + "2.Modificar Distancia de la Tierra\n"
+                            + "Elegir una opcion: ");
+                            pochita = new Scanner(System.in);
+                            opc = pochita.nextInt();
+                            switch (opc) {
+                                case 1:
+                                    
+                                    break;
+                                case 2:
+                                    break;
+                                default:
+                                    throw new AssertionError();
+                            }
+                            break;
+                            //fin opc 3
+                        case 4:
+                            System.out.print("---Aereo normal---\n\n"
+                            + "1.Modificar Cantidad Maxima de Pasajeros\n"
+                            + "3.Modificar Longitud Normal\n"
+                            + "4.Modificar Cantidad de Motores\n"
+                            + "5.Modificar Grupo Sanguineo\n"
+                            + "6.Modificar Sexo\n"
+                            + "7.Modificar Altura\n"
+                            + "8.Modificar Peso\n"
+                            + "Elegir una opcion: ");
+                            break;
+                            //fin opc 4.
+                        case 5:
+                            break;
+                            //fin opc 5
+                        default:
+                            throw new AssertionError();
+                    }
                     break;
                     // fin opc 2
                 case 3:
