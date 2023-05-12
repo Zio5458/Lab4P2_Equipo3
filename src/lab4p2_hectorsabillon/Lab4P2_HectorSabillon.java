adab165bbd5f59de8a0e5aea59701cb2eef4e8e8
Hector Sabillon <hdannysabillon992232@gmail.com>
2023-05-12 16:58:00 -0600

    una cosita mas

--- src/lab4p2_hectorsabillon/Lab4P2_HectorSabillon.java
+++ src/lab4p2_hectorsabillon/Lab4P2_HectorSabillon.java
@@ -22,7 +22,7 @@ public class Lab4P2_HectorSabillon {
                     + "6.Salir\n"
                     + "Elegir una opcion: ");
             opc = pochita.nextInt();
-            System.out.println("");
+            pochite.nextLine();
             switch (opc) {
                 case 1:
                     System.out.print("---Crear---\n\n"
